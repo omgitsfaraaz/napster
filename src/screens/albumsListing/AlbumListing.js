6a726178433590856e6dcef2979a9a1d8dcbd9f3
omgitsfaraaz <mohsinsait23@gmail.com>
2023-01-15 20:20:38 +0530

    album list ui done

--- src/screens/albumsListing/AlbumListing.js
+++ src/screens/albumsListing/AlbumListing.js
@@ -1,36 +1,59 @@
 import {
   View,
   Text,
-  SafeAreaView,
   Dimensions,
-  Image,
   FlatList,
+  TouchableOpacity,
+  Image,
 } from 'react-native';
 import React from 'react';
 import {styles} from './styles';
+import FastImage from 'react-native-fast-image';
 
 const windowWidth = Dimensions.get('window').width;
 
-const AlbumListing = props => {
+const AlbumListing = ({navigation}) => {
   const albums = [1, 2, 3];
+
+  const renderEachAlbum = () => {
+    return (
+      <TouchableOpacity
+        style={{
+          width: windowWidth / 2.5,
+          marginBottom: 30,
+        }}
+        onPress={() => navigation.navigate('IndividualAlbum')}>
+        <View style={styles.cd}></View>
+        <View style={{position: 'absolute', borderRadius: 20}}>
+          <Image
+            source={{
+              uri: 'https://images.unsplash.com/photo-1524650359799-842906ca1c06?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80',
+            }}
+            style={{width: windowWidth / 3, height: 100, borderRadius: 10}}
+          />
+        </View>
+        <View>
+          <Text>Kantara (May 2019)</Text>
+          <Text>Harry styles, Joshua</Text>
+        </View>
+      </TouchableOpacity>
+    );
+  };
   return (
     <View>
       <View style={{backgroundColor: 'red'}}>
         <Text style={{textAlign: 'center', fontSize: 25}}>Popular Albums</Text>
       </View>
 
-      <View>
-        <View style={{width: windowWidth / 2.5}}>
-          <View style={styles.cd}></View>
-          <View style={{position: 'absolute', borderRadius: 20}}>
-            <Image
-              source={{
-                uri: 'https://images.unsplash.com/photo-1524650359799-842906ca1c06?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80',
-              }}
-              style={{width: windowWidth / 3, height: 100, borderRadius: 10}}
-            />
-          </View>
-        </View>
+      <View style={{width: '90%', alignSelf: 'center', marginTop: 20}}>
+        <FlatList
+          data={albums}
+          renderItem={renderEachAlbum}
+          keyExtractor={item => item}
+          horizontal={false}
+          numColumns={2}
+          columnWrapperStyle={{justifyContent: 'space-between'}}
+        />
       </View>
     </View>
   );
